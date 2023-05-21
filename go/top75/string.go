93ae9f23c03874d61980ff4655d9c3e17c13ba99
zhangxiaohe <zhangxiaohe@zhihu.com>
2023-05-21 20:12:04 +0800

    reverseVowels

--- go/top75/string.go
+++ go/top75/string.go
@@ -1,5 +1,7 @@
 package top75
 
+import "strings"
+
 // GcdOfStrings 1071. 字符串的最大公因子
 // 对于字符串s 和t，只有在s = t + ... + t（t 自身连接 1 次或多次）时，我们才认定“t 能除尽 s”。
 // 给定两个字符串str1和str2。返回 最长字符串x，要求满足x 能除尽 str1 且 x 能除尽 str2 。
@@ -40,4 +42,91 @@ func findGcd(short string, long string, index int) string {
 		return indexStr
 	}
 	return ""
+}
+
+// kidsWithCandies 1431. 拥有最多糖果的孩子
+//给你一个数组candies和一个整数extraCandies，其中candies[i]代表第 i 个孩子拥有的糖果数目。
+//
+//对每一个孩子，检查是否存在一种方案，将额外的extraCandies个糖果分配给孩子们之后，此孩子有 最多的糖果。
+//注意，允许有多个孩子同时拥有 最多的糖果数目。
+//输入：candies = [2,3,5,1,3], extraCandies = 3
+//输出：[true,true,true,false,true]
+//解释：
+//孩子 1 有 2 个糖果，如果他得到所有额外的糖果（3个），那么他总共有 5 个糖果，他将成为拥有最多糖果的孩子。
+//孩子 2 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+//孩子 3 有 5 个糖果，他已经是拥有最多糖果的孩子。
+//孩子 4 有 1 个糖果，即使他得到所有额外的糖果，他也只有 4 个糖果，无法成为拥有糖果最多的孩子。
+//孩子 5 有 3 个糖果，如果他得到至少 2 个额外糖果，那么他将成为拥有最多糖果的孩子。
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	result := make([]bool, len(candies))
+	max := 0
+	for i := 0; i < len(candies); i++ {
+		if max < candies[i] {
+			max = candies[i]
+		}
+	}
+	for i := 0; i < len(candies); i++ {
+		if max <= extraCandies+candies[i] {
+			result[i] = true
+		}
+	}
+	return result
+}
+
+// canPlaceFlowers 605. 种花问题
+// 假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+//
+//给你一个整数数组flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。
+//另有一个数n ，能否在不打破种植规则的情况下种入n朵花？能则返回 true ，不能则返回 false。
+// flowerbed = [1,0,0,0,1], n = 1 -> true
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	count := 0
+	if len(flowerbed) == 1 {
+		return 1-flowerbed[0] >= n
+	}
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 1 {
+			continue
+		}
+		if i == 0 {
+			if flowerbed[i+1] == 0 {
+				flowerbed[i] = 1
+				count++
+			}
+		} else if flowerbed[i-1] == 0 {
+			if i == len(flowerbed)-1 {
+				flowerbed[i] = 1
+				count++
+			} else if flowerbed[i+1] == 0 {
+				flowerbed[i] = 1
+				count++
+			}
+		}
+	}
+	return count >= n
+}
+
+// reverseVowels 345. 反转字符串中的元音字母
+// 给你一个字符串 s ，仅反转字符串中的所有元音字母，并返回结果字符串。
+// 元音字母包括 'a'、'e'、'i'、'o'、'u'，且可能以大小写两种形式出现不止一次。
+func reverseVowels(s string) string {
+	if len(s) == 1 {
+		return s
+	}
+	left, right := 0, len(s)-1
+	result := []byte(s)
+	for left < right {
+		if left < len(s) && !strings.Contains("aeiouAEIOU", string(s[left])) {
+			left++
+		}
+		if right > 0 && !strings.Contains("aeiouAEIOU", string(s[right])) {
+			right--
+		}
+		if left < right {
+			result[left], result[right] = result[right], result[left]
+			left++
+			right--
+		}
+	}
+	return string(result)
 }
--- go/top75/string_test.go
+++ go/top75/string_test.go
@@ -1,7 +1,22 @@
 package top75
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGcdOfStrings(t *testing.T) {
 	println("---------" + GcdOfStrings("abcabc", "abc"))
+}
+
+func TestKidsWithCandies(t *testing.T) {
+	fmt.Printf("---------%v", kidsWithCandies([]int{2, 3, 5, 1, 3}, 3))
+}
+
+func TestCanPlaceFlowers(t *testing.T) {
+	fmt.Printf("-------%v\n", canPlaceFlowers([]int{0, 1, 0}, 1))
+}
+
+func TestReverseVowels(t *testing.T) {
+	fmt.Printf("-----%v\n", reverseVowels("hello"))
 }
